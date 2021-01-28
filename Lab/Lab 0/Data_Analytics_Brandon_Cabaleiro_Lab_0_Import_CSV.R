999fbdea6d19b6b2297df127ad3edd6e89b8d128
brandon-cabaleiro <59896044+brandon-cabaleiro@users.noreply.github.com>
2021-01-28 16:19:39 -0500

    Add files via upload

--- Lab/Lab 0/Data_Analytics_Brandon_Cabaleiro_Lab_0_Import_CSV.R
+++ Lab/Lab 0/Data_Analytics_Brandon_Cabaleiro_Lab_0_Import_CSV.R
@@ -1,6 +1,6 @@
 # This practices the use of importing CSV files.
 
 help(read.csv)
-data1 <- read.csv(file.choose(), header=T)
+data1 <- read.csv(file.choose(), header=T) # allows you to choose a file by opening your file explorer
 
 head(data1)
