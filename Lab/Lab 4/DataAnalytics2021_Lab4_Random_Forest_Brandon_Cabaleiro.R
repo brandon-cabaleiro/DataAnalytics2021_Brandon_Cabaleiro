57fcbc6ed9cd0656c5a1dcb345e73eb89e59063f
brandon-cabaleiro <59896044+brandon-cabaleiro@users.noreply.github.com>
2021-03-19 13:57:39 -0400

    Add files via upload

--- Lab/Lab 4/DataAnalytics2021_Lab4_Random_Forest_Brandon_Cabaleiro.R
+++ Lab/Lab 4/DataAnalytics2021_Lab4_Random_Forest_Brandon_Cabaleiro.R
@@ -11,19 +11,45 @@
 #Hi Mukesh and Professor Thilanka, still working on this lab.
 
 library(randomForest)
-titanic <- data("Titanic")
+library(titanic)
 
-summary(titanic::titanic_train)
-summary(titanic::titanic_test)
+head(titanic_train)
 
-model1 <- randomForest(Survived ~ ., data = titanic::titanic_train, importance = TRUE)
+# cleaning up the data
+titanic_train[titanic_train == ""] <- NA
+
+titanic_train[titanic_train$Survived == 0,]$Survived <- "No"
+titanic_train[titanic_train$Survived == 1,]$Survived <- "Yes"
+titanic_train$Survived <- as.factor(titanic_train$Survived)
+
+titanic_train$Pclass <- as.factor(titanic_train$Pclass)
+
+titanic_train[titanic_train$Sex == "female",]$Sex <- "F"
+titanic_train[titanic_train$Sex == "male",]$Sex <- "M"
+titanic_train$Sex <- as.factor(titanic_train$Sex)
+
+titanic_train$Embarked <- as.factor(titanic_train$Embarked)
+
+# showing the structure of the data
+str(titanic_train)
+
+# creating another frame for columns with only nums and factors for rfImpute
+titanic_train_no_char <- titanic_train[c(1:3, 5:8, 10, 12)]
+
+set.seed(50)
+
+# impute the NA values
+titanic_train_imputed <- rfImpute(Survived ~ ., data=titanic_train_no_char, iter=6)
+
+# creating two models of randomForest
+model1 <- randomForest(Survived ~ ., data = titanic_train_imputed, importance = TRUE)
 model1
 
-model2 <- randomForest(Survived ~ ., data = titanic::titanic_train, ntree=500, mtry=6, importance = TRUE)
+model2 <- randomForest(Survived ~ ., data = titanic_train_imputed, ntree=1000, mtry=6, importance = TRUE)
 model2
 
-predTrain <- predict(model2, titanic::titanic_train, type="class")
-table(predTrain, titanic::titanic_train$Survived)
+# output predictions
+predTrain <- predict(model1, titanic_train_imputed, type="class")
+table(predTrain, titanic_train_imputed$Survived)
 
-predValid <- predict(model2, titanic::titanic_test, type="class")
-table(predTrain, titanic::titanic_train$Survived)
+
